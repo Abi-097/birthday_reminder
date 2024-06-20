d1b7cd8773a8a3e35a585fc5d53dfb3ebdbf1c2e
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-06-20 23:06:18 +0530

    signin added

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -1,88 +1,9 @@
-import { Card, TextField, Typography } from "@mui/material";
-import Image from "next/image";
-import CommonButton from "@/app/components/common/Button";
-import { useForm } from "react-hook-form";
-const Register = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100%",
-        backgroundColor: "#f0f0f0",
-        borderRadius: 25,
-      }}
-    >
-      <Card
-        sx={{
-          width: "400px",
-          height: "450px",
-          backgroundColor: "#F3EEEA",
-          padding: "20px",
-          borderRadius: "15px",
-          boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.1)",
-        }}
-      >
-        <Image
-          src="/logo.png"
-          alt="logo.png"
-          width={100}
-          height={100}
-          style={{ display: "block", margin: "auto", marginBottom: "12px" }}
-        />
-        {"register" === "register" && (
-          <TextField
-            id="outlined-basic"
-            label="Name"
-            color="info"
-            variant="outlined"
-            sx={{ width: "100%", mb: 2 }}
-            {...register("username", {
-              required: "Name is required",
-              validate: (value) => {
-                if (value.length < 3) {
-                  return "Name must have at least 3 Characters";
-                }
-              },
-            })}
-          />
-        )}
+import Form from "@/app/components/Form";
 
-        <TextField
-          id="outlined-basic"
-          label="Email"
-          color="info"
-          variant="outlined"
-          sx={{ width: "100%", mb: 2 }}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Password"
-          color="info"
-          variant="outlined"
-          sx={{ width: "100%", mb: 4 }}
-        />
-        <CommonButton
-          color="warning"
-          variant="contained"
-          sx={{ display: "block", margin: "auto", mb: 2 }}
-        >
-          Sign In
-        </CommonButton>
-        <Typography>
-          Dont have account? Create{" "}
-          <span style={{ color: "green", fontWeight: 600 }}>here</span>
-        </Typography>
-      </Card>
-    </div>
-  );
+const Register = () => {
+  <div>
+    <Form type="register" /> heelo
+  </div>;
 };
 
 export default Register;
--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Register from "./(auth)/register/page";
 
 export default function Home() {
   return (
+    // <div>
     <div className="container">
       <Register />
     </div>
